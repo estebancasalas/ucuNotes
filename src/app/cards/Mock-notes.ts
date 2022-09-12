bcf820f9dbee9abee6d6e1a3e9fb45eecea743bc
Néstor Martínez <nestor.marvi@gmail.com>
2022-09-12 19:51:00 -0300

    agregado formato

--- src/app/cards/Mock-notes.ts
+++ src/app/cards/Mock-notes.ts
@@ -1,5 +1,8 @@
 import { Note } from './Note';
 
 export const NOTES: Note[] = [
-    {'id':1, 'city':'Berlin', 'date':'2022-09-13', 'time':'12:00', 'text':'Generado Automaticamente'},{'id':2, 'city':'Montevideo', 'date':'2022-09-13', 'time':'14:00', 'text':'Generado Automaticamente'}
+    {'id':1, 'city':'Berlin', 'date':'2022-09-13', 'time':'12:00', 'text':'Generado Automaticamente'},
+    {'id':2, 'city':'Madrid', 'date':'2022-09-13', 'time':'12:00', 'text':'Generado Automaticamente'},
+    {'id':3, 'city':'Washington', 'date':'2022-09-13', 'time':'12:00', 'text':'Generado Automaticamente'},{'id':4, 'city':'Montevideo', 'date':'2022-09-13', 'time':'14:00', 'text':'Generado Automaticamente'}
+
 ];
